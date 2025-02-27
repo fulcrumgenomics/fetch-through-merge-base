db7d6e3fca7cb64f67a16cddaa0bb225b1776d70
Nils Homer <nilshomer@gmail.com>
2025-02-26 23:21:51 -0500

    feat: update and unshallow when fetching

--- src/scripts/fetch_through_merge_base.sh
+++ src/scripts/fetch_through_merge_base.sh
@@ -27,23 +27,23 @@ set -eou pipefail
 
 # Fetch a branch or tag, and track it.  Do not fetch if a commit (yet).
 if [[ "${GITHUB_BASE_REF}" != "$(git rev-parse --verify ${GITHUB_BASE_REF})" ]]; then
-    git fetch --update-head-ok --progress --quiet --depth=1 origin "$GITHUB_BASE_REF:$GITHUB_BASE_REF";
+    git fetch --update-head-ok --update-shallow --progress --quiet --depth=1 origin "$GITHUB_BASE_REF:$GITHUB_BASE_REF";
 fi
 if [[ "${GITHUB_HEAD_REF}" != "$(git rev-parse --verify ${GITHUB_HEAD_REF})" ]]; then
-    git fetch --update-head-ok --progress --quiet --depth=1 origin "$GITHUB_HEAD_REF:$GITHUB_HEAD_REF";
+    git fetch --update-head-ok --update-shallow --progress --quiet --depth=1 origin "$GITHUB_HEAD_REF:$GITHUB_HEAD_REF";
 fi
 
 # Fetch the references and track them in temporary branches.  Hopefully there are no collisions!
-git fetch --progress --quiet --depth=1 origin "$GITHUB_BASE_REF:__github_base_ref__";
-git fetch --progress --quiet --depth=1 origin "$GITHUB_HEAD_REF:__github_head_ref__";
+git fetch --progress --quiet --depth=1 --update-shallow origin "$GITHUB_BASE_REF:__github_base_ref__";
+git fetch --progress --quiet --depth=1 --update-shallow origin "$GITHUB_HEAD_REF:__github_head_ref__";
 GITHUB_BASE_REF=$(git rev-parse "__github_base_ref__");
 GITHUB_HEAD_REF=$(git rev-parse "__github_head_ref__");
 
 # keep fetching deeper until we find the common ancestor reference
 while [ -z "$( git merge-base "__github_base_ref__" "__github_head_ref__" )" ]; do
   # fetch deeper
-  git fetch  --quiet --deepen="$DEEPEN_LENGTH" origin "$GITHUB_HEAD_REF";
-  git fetch  --quiet --deepen="$DEEPEN_LENGTH" origin "$GITHUB_BASE_REF";
+  git fetch  --quiet --update-shallow --deepen="$DEEPEN_LENGTH" origin "$GITHUB_HEAD_REF";
+  git fetch  --quiet --update-shallow --deepen="$DEEPEN_LENGTH" origin "$GITHUB_BASE_REF";
   python ${SCRIPT_DIR}/git_ungraft.py;
   # check if we are done iterating
   set +e;
@@ -55,7 +55,7 @@ while [ -z "$( git merge-base "__github_base_ref__" "__github_head_ref__" )" ]; 
       exit 1;
     fi
     echo "Falling back to fetching all references";
-    git fetch --quiet --all;
+    git fetch --quiet --all --unshallow;
     python ${SCRIPT_DIR}/git_ungraft.py;
     break;
   else
